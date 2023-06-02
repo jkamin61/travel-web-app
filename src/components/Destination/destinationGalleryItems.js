613f3ff97ef01a0f2ba56737a9218892dc74a6d3
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2023-06-02 22:02:11 +0200

    - created array with images for destination section

---
+++ src/components/Destination/destinationGalleryItems.js
@@ -0,0 +1,33 @@
+export const destinationGalleryItems = [
+    {
+        image: 'https://ibb.co/d0nKQmz',
+        description: 'Taman Nasional Komodo',
+        days: '7'
+    },
+     {
+        image: 'https://ibb.co/x2pdrBM',
+        description: 'Taman Nasional Bunaken',
+        days: '9'
+    },
+     {
+        image: 'https://ibb.co/QD5J7b9',
+        description: 'Raja Ampat, Papua Barat',
+        days: '14'
+    },
+     {
+        image: 'https://ibb.co/gmKNVP0',
+        description: 'Kepulauan Wakatobi',
+        days: '12'
+    },
+     {
+        image: 'https://ibb.co/HpMwv2y',
+        description: 'Gili Trawangan, Lombok',
+        days: '7'
+    },
+     {
+        image: 'https://ibb.co/jHndprS',
+        description: 'Taman Nasional Bromo',
+        days: '7'
+    },
+
+]
