dbba014c96b4d472def4922e6fbf3471e8a03d81
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2023-06-04 03:20:50 +0200

    added mobile version styling for destination section

--- src/components/Destination/Destination.jsx
+++ src/components/Destination/Destination.jsx
@@ -1,30 +1,49 @@
 import css from "./Destination.module.css"
+import {destinationGalleryItems} from "./destinationGalleryItems";
+import React, {useRef} from 'react';
 
 export const Destination = () => {
+    const photoContainerRef = useRef(null);
+    let currentIndex = 0;
+    const scrollPhotos = () => {
+        const photoContainer = photoContainerRef.current;
+        if (!photoContainer) return;
+
+        const photoWidth = photoContainer.offsetWidth;
+        const newPosition = -currentIndex * photoWidth;
+        photoContainer.style.transform = `translateX(${newPosition}px)`;
+    }
+    const handlePreviousButton = () => {
+        if (currentIndex > 0) {
+            currentIndex--;
+            scrollPhotos();
+        }
+    }
+    const handleNextButton = () => {
+        const photoContainer = photoContainerRef.current;
+        if (!photoContainer) return;
+        if (currentIndex < photoContainer.children.length - 1) {
+            currentIndex++;
+            scrollPhotos();
+        }
+    }
     return (
         <section className={css.destination}>
             <h3>Top Destination</h3>
             <h2>Discover your love</h2>
-            <ul>
-                <li>
-                    <img alt={"pic"}/>
-                </li>
-                <li>
-                    <img alt={"pic"}/>
-                </li>
-                <li>
-                    <img alt={"pic"}/>
-                </li>
-                <li>
-                    <img alt={"pic"}/>
-                </li>
-                <li>
-                    <img alt={"pic"}/>
-                </li>
-                <li>
-                    <img alt={"pic"}/>
-                </li>
-            </ul>
+            <div className={css.photoBox}>
+                <button className={`${css.arrowBtn} ${css.prevBtn}`} onClick={handlePreviousButton}>&#8249;</button>
+                <div ref={photoContainerRef} className={css.photoContainer}>
+                    {destinationGalleryItems.map(element => {
+                        return (
+                            <>
+                                <img src={element.image} alt={element.description}/>
+                            </>
+                        )
+                    })}
+                </div>
+                <button className={`${css.arrowBtn} ${css.nextBtn}`} onClick={handleNextButton}>&#8250;</button>
+            </div>
         </section>
     )
 }
--- src/components/Destination/destinationGalleryItems.js
+++ src/components/Destination/destinationGalleryItems.js
@@ -1,31 +1,31 @@
 export const destinationGalleryItems = [
     {
-        image: 'https://ibb.co/d0nKQmz',
+        image: 'https://i.ibb.co/kgYchmW/destination-4.png',
         description: 'Taman Nasional Komodo',
         days: '7'
     },
      {
-        image: 'https://ibb.co/x2pdrBM',
+        image: 'https://i.ibb.co/D1xTjv7/destination-3.png',
         description: 'Taman Nasional Bunaken',
         days: '9'
     },
      {
-        image: 'https://ibb.co/QD5J7b9',
+        image: 'https://i.ibb.co/tLdHwmB/destination-2.png',
         description: 'Raja Ampat, Papua Barat',
         days: '14'
     },
      {
-        image: 'https://ibb.co/gmKNVP0',
+        image: 'https://i.ibb.co/4KrzdYb/destination-1.png',
         description: 'Kepulauan Wakatobi',
         days: '12'
     },
      {
-        image: 'https://ibb.co/HpMwv2y',
+        image: 'https://i.ibb.co/4gDvyfL/destination-6.png',
         description: 'Gili Trawangan, Lombok',
         days: '7'
     },
      {
-        image: 'https://ibb.co/jHndprS',
+        image: 'https://i.ibb.co/25GwbkT/destination-5.png',
         description: 'Taman Nasional Bromo',
         days: '7'
     },
