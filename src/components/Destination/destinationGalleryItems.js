13df44c1f624803f5f72035337c4388f87198ffd
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2023-06-04 16:36:54 +0200

    #small fixes

--- src/components/Destination/destinationGalleryItems.js
+++ src/components/Destination/destinationGalleryItems.js
@@ -1,30 +1,36 @@
 export const destinationGalleryItems = [
     {
+        id: '1',
         image: 'https://i.ibb.co/kgYchmW/destination-4.png',
         description: 'Taman Nasional Komodo',
         days: '7'
     },
      {
+         id: '2',
         image: 'https://i.ibb.co/D1xTjv7/destination-3.png',
         description: 'Taman Nasional Bunaken',
         days: '9'
     },
      {
+         id: '3',
         image: 'https://i.ibb.co/tLdHwmB/destination-2.png',
         description: 'Raja Ampat, Papua Barat',
         days: '14'
     },
      {
+         id: '4',
         image: 'https://i.ibb.co/4KrzdYb/destination-1.png',
         description: 'Kepulauan Wakatobi',
         days: '12'
     },
      {
+         id: '5',
         image: 'https://i.ibb.co/4gDvyfL/destination-6.png',
         description: 'Gili Trawangan, Lombok',
         days: '7'
     },
      {
+         id: '6',
         image: 'https://i.ibb.co/25GwbkT/destination-5.png',
         description: 'Taman Nasional Bromo',
         days: '7'
--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,41 +1,22 @@
 import css from './Testimonial.module.css';
-
+import {avatars} from "./avatars";
+import {nanoid} from 'nanoid';
 export const Testimonial = () => {
     return (
         <section className={css.testimonial}>
-            <h3>Testimonial</h3>
-            <h2>What they say about us</h2>
-            <ul>
-                <li>
-                    <img alt={"avatar"}/>
-                    <p>Name</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis</p>
-                </li>
-                <li>
-                    <img alt={"avatar"}/>
-                    <p>Name</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis</p>
-                </li>
-                <li>
-                    <img alt={"avatar"}/>
-                    <p>Name</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis</p>
-                </li>
-                <li>
-                    <img alt={"avatar"}/>
-                    <p>Name</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis</p>
-                </li>
-                <li>
-                    <img alt={"avatar"}/>
-                    <p>Name</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis</p>
-                </li>
-                <li>
-                    <img alt={"avatar"}/>
-                    <p>Name</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis</p>
-                </li>
+            <h3 className={css.heading}>Testimonial</h3>
+            <h2 className={css.subHeading}>What they say about us</h2>
+            <ul className={css.commentsList}>{
+                avatars.map(element => {
+                    return (
+                        <li key={nanoid()} className={css.commentItem}>
+                            <img src={element.avatar} alt={'avatar'} className={css.avatar}/>
+                            <h3 className={css.usersName}>{element.name}</h3>
+                            <p className={css.comment}>{element.comment}</p>
+                        </li>
+                    )
+                })
+            }
             </ul>
         </section>
     )
