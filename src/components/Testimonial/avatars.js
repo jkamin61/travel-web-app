2bef18c2728bd2704b62a4a29c4ed7c9dd069bee
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2023-06-04 16:35:46 +0200

    added list of avatars for Testimonial section

---
+++ src/components/Testimonial/avatars.js
@@ -0,0 +1,39 @@
+export const avatars = [
+    {
+        id: '1',
+        name: 'Angelina Simple',
+        avatar: 'https://i.ibb.co/R9JC51b/avatar-0.png',
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis',
+    },
+    {
+        id: '2',
+        name: 'Viola Natalie',
+        avatar: 'https://i.ibb.co/R6rzTrx/avatar-1.png',
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis',
+    },
+    {
+        id: '3',
+        name: 'Robert John K.',
+        avatar: 'https://i.ibb.co/xYmXNfy/avatar-2.png',
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis',
+    },
+    {
+        id: '4',
+        name: 'Anggi Nani',
+        avatar: 'https://i.ibb.co/fQjtgQY/avatar-3.png',
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis',
+    },
+    {
+        id: '5',
+        name: 'Graham Suryo J',
+        avatar: 'https://i.ibb.co/bH1XxV3/avatar-4.png',
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis',
+    },
+    {
+        id: '6',
+        name: 'Bagas G Natfi',
+        avatar: 'https://i.ibb.co/rmXrqC3/avatar-5.png',
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis',
+    },
+
+]
