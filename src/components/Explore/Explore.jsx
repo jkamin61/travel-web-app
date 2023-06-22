197f466a55057e4809206bd3759847aa3bfefedf
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2023-06-22 02:07:11 +0200

    mui icons for form

--- src/components/Explore/Explore.jsx
+++ src/components/Explore/Explore.jsx
@@ -1,4 +1,6 @@
 import css from './Explore.module.css';
+import SearchIcon from '@mui/icons-material/Search';
+import PlaceIcon from '@mui/icons-material/Place';
 
 export const Explore = () => {
     const handleExploreButtonAnimation = (event) => {
@@ -14,7 +16,7 @@ export const Explore = () => {
     return (
         <section className={css.explore}>
             <div className={css.mainText}>
-                <h2>Explore the world with a smile</h2>
+                <h2 className={css.exploreHeading}>Explore the world with a smile</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus
                     venenatis,
                     lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis
@@ -22,10 +24,15 @@ export const Explore = () => {
                 </p>
             </div>
             <form className={css.form}>
-                    <input type={"search"}  className={css.exploreInputs}></input>
-                    <input type={"date"} className={css.exploreInputs}></input>
-                    <input type={"search"} className={css.exploreInputs}></input>
-                    <button type={"submit"} className={css.exploreSubmitButton} onClick={handleFormSubmit} onMouseDown={handleExploreButtonAnimation} onMouseUp={handleExploreButtonAnimation}>FIND TRIP NOW</button>
+                <div>
+                <PlaceIcon></PlaceIcon>
+                </div>
+                <input type={"search"} className={css.exploreInputs}></input>
+                <input type={"date"} className={css.exploreInputs}></input>
+                <input type={"search"} className={css.exploreInputs}></input>
+                <button type={"submit"} className={css.exploreSubmitButton} onClick={handleFormSubmit}
+                        onMouseDown={handleExploreButtonAnimation} onMouseUp={handleExploreButtonAnimation}>FIND TRIP
+                    NOW<SearchIcon></SearchIcon></button>
             </form>
         </section>
     )
